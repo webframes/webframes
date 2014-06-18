9c199c603940c84b3232be65833594cbd698ee82
Steven Vachon <contact@svachon.com>
2014-06-17 20:45:12 -0400

    matte'ing now export-only, container test

--- test/options.js
+++ test/options.js
@@ -5,16 +5,38 @@ var util   = require("./util");
 
 describe("Options", function()
 {
+	/*it.only("BUILD FILES", function(done)
+	{
+		require("async").parallel(
+		[
+			function(callback)
+			{
+				// Container expected SVG result
+				util.run(
+				{
+					options: { folder:util.resolvePath("2-frames/opaque/gif"), export:util.resolvePath("2-frames/opaque/gif-expected-export-(contained).svg"), contain:true },
+					callback: callback
+				});
+			}
+		],
+		function(error)
+		{
+			done();
+		});
+	});*/
+	
+	
+	
 	it("contain", function(done)
 	{
-		// Compare size to expected SVG result (unminified SMIL)
+		// Compare size to expected SVG result (unminified and contained SMIL)
 		util.run(
 		{
 			options: { folder:util.resolvePath("2-frames/opaque/gif"), export:true, contain:true },
-			expected: "2-frames/opaque/gif-expected-export.svg",
+			expected: "2-frames/opaque/gif-expected-export-(contained).svg",
 			callback: function(error, result, expectedResult)
 			{
-				expect( util.sizeOf(result.export) ).to.be.above( util.sizeOf(expectedResult) );
+				expect(result.export).to.equal( expectedResult.toString() );
 				done();
 			}
 		});
